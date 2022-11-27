881921ad50419a6a9f6bf2778d888261e1a0436f
“kritik <kritikjain9@gmail.com>
2022-11-27 08:15:54 +0530

    Added author name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+const authorName = document.getElementById('author')
 
-const res = fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=sports')
+
+
+const res = fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=football')
 const data = res.then(res => res.json())
 const image = data.then((imageData) => {
-    document.body.style.backgroundImage = `url('${imageData.urls.full}')`;
+    // console.log(imageData);
+
+    const imageUrl = imageData.urls.full
+    document.body.style.backgroundImage = `url('${imageUrl}')`
+
+    const author = imageData.user.first_name
+    authorName.textContent = `By : ${author}`
 })
 
