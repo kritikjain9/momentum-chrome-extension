bc80bdc8a9d51922c56bbb4adaffb69e8453a1c4
“kritik <kritikjain9@gmail.com>
2022-11-29 02:54:20 +0530

    Added the default background in case API fails

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ const authorName = document.getElementById('author')
 
 
 
-const res = fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=football')
+const res = fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=puppies')
 const data = res.then(res => res.json())
 const image = data.then((imageData) => {
     // console.log(imageData);
 
+    console.log(imageData.urls.full);
     const imageUrl = imageData.urls.full
     document.body.style.backgroundImage = `url('${imageUrl}')`
 
     const author = imageData.user.first_name
     authorName.textContent = `By : ${author}`
 })
+    .catch(err => {
+        console.log(err);
+        const defaultImageUrl = "https://images.unsplash.com/photo-1506780685701-ba404b9e640d?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Njk2NzAzNDk&ixlib=rb-4.0.3&q=80"
+        document.body.style.backgroundImage = `url('${defaultImageUrl}')`
+    })
 
